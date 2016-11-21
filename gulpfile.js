cfb64b75c3dcce403f9765327680d00a17dd8bbe
Yaroslav Lozhkin <yalozhkin@gmail.com>
2016-11-21 16:51:15 +0400

    Get stable version
    * Need to improve images processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,102 @@
-var browserSync = require('browser-sync');
-var gulp = require('gulp');
-var include = require('gulp-include');
-var plumber = require('gulp-plumber');
-var pug = require('gulp-pug');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+var
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync'),
+  gulp = require('gulp'),
+  imagemin = require('gulp-imagemin'),
+  include = require('gulp-include'),
+  plumber = require('gulp-plumber'),
+  pngquant = require('imagemin-pngquant'),
+  pug = require('gulp-pug'),
+  pump = require('pump'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass'),
+  sassGlob = require('gulp-sass-glob'),
+  uglify = require('gulp-uglify'),
+  watch = require('gulp-watch');
 
-// Pug builds HTML views
+// Views
 
 gulp.task('pug', function() {
-    gulp.src([
-            'source/views/**/*.pug',
-            '!source/views/**/_*.pug',
-            '!source/views/home/**',
-            '!source/views/layouts/**',
-            '!source/views/shared/**',
-        ])
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true,
-        }))
-        .pipe(gulp.dest('build/'))
-        .pipe(browserSync.stream());
+  gulp.src([
+      'source/views/**/*.pug',
+      '!source/views/**/_*.pug',
+      '!source/views/home/**',
+      '!source/views/layouts/**',
+      '!source/views/shared/**',
+    ])
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true,
+    }))
+    .pipe(gulp.dest('build/'))
+    .pipe(browserSync.stream());
 
-    gulp.src('source/views/home/*.pug')
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true,
-        }))
-        .pipe(gulp.dest('build/'))
-        .pipe(browserSync.stream());
+  gulp.src('source/views/home/*.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true,
+    }))
+    .pipe(gulp.dest('build/'))
+    .pipe(browserSync.stream());
 });
 
-// SASS builds CSS stylesheet
+// Styles
 
 gulp.task('sass', function() {
-    gulp.src([
-            'source/styles/*.sass'
-        ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            indentedSyntax: true,
-            errLogToConsole: true,
-            sync: true
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('build/css/'))
-        .pipe(browserSync.stream());
+  gulp.src('source/styles/main.scss')
+    .pipe(plumber())
+    .pipe(sassGlob())
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths,
+      indentedSyntax: true,
+      errLogToConsole: true,
+      sync: true
+    }))
+    .pipe(gulp.dest('build/assets/styles/'))
+    .pipe(browserSync.stream());
+});
+
+// Images
+
+gulp.task('imagemin', function() {
+  gulp.src('source/images/**/*')
+    .pipe(plumber())
+    .pipe(imagemin({
+      progressive: true,
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('build/assets/images/'));
+});
+
+// Javascript
+
+gulp.task('uglify', function(cb) {
+  pump([
+      gulp.src('scripts/*')
+      .pipe(uglify()),
+      gulp.dest('build/assets/scripts/')
+    ],
+    cb
+  );
 });
 
 
-// Browsersync server
+// Browsersync starts server
 
 gulp.task('server', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'build'
-        },
-        open: false
-    });
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+    open: false
+  });
 });
 
-// Default task runs server and watch changes
+// Default task watch changes and update on server
 
 gulp.task('default', ['server'], function() {
-    gulp.watch('source/views/**/*', ['pug']);
-    gulp.watch('source/styles/**/*', ['sass']);
+  gulp.watch('source/views/**/*', ['pug']);
+  gulp.watch('source/styles/**/*', ['sass']);
+  gulp.watch('source/scripts/**/*', ['uglify']);
+  gulp.watch('source/images/**/*', ['imagemin']);
 });
